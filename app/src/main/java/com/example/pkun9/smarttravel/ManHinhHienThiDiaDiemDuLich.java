f4fe87446a6b40a00a4cb03b15f9c4b8838bc211
Coder <phamhoangdu95@gmail.com>
2018-06-08 22:23:44 +0700

    Man hình hiển thị địa điểm du lịch

--- app/src/main/java/com/example/pkun9/smarttravel/ManHinhHienThiDiaDiemDuLich.java
+++ app/src/main/java/com/example/pkun9/smarttravel/ManHinhHienThiDiaDiemDuLich.java
@@ -4,12 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ManHinhHienThiDiaDiemDuLich extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_hien_thi_dia_diem_du_lich);
 
     }
 }
