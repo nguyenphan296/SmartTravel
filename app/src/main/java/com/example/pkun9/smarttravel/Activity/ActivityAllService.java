f17456cfba476903d02707d4386893b21f8e1819
nguyenphan296 <nguyenphan2997@gmail.com>
2018-06-27 23:26:19 +0700

    update

--- app/src/main/java/com/example/pkun9/smarttravel/Activity/ActivityAllService.java
+++ app/src/main/java/com/example/pkun9/smarttravel/Activity/ActivityAllService.java
@@ -10,10 +10,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.pkun9.smarttravel.Fragment.FragmentDiaDiem;
 import com.example.pkun9.smarttravel.Fragment.FragmentHotel;
 import com.example.pkun9.smarttravel.Fragment.FragmentLanguage;
 import com.example.pkun9.smarttravel.Fragment.FragmentRestaurant;
+import com.example.pkun9.smarttravel.Fragment.FragmentTaxi;
 import com.example.pkun9.smarttravel.R;
 
 /**
@@ -28,69 +28,68 @@ import com.example.pkun9.smarttravel.R;
  * @since 1.0
  */
 public class ActivityAllService extends AppCompatActivity {
-    //TextView txt_language;
+    TextView txt_language;
     TextView txt_restaurant;
     TextView txt_hotel;
-    TextView txt_language,txt_DiaDiem;
-
+    TextView txt_taxi;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_all_service);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_all_service );
         init();
         initEvent();
 
     }
 
     private void initEvent() {
-        txt_language.setOnClickListener(new View.OnClickListener() {
+        txt_language.setOnClickListener( new View.OnClickListener() {
             @SuppressLint("ResourceAsColor")
             @Override
             public void onClick(View v) {
                 Fragment fragment = new FragmentLanguage();
-                add_fragment(fragment);
-             //   txt_language.setBackgroundColor(Color.RED);
-                txt_language.setBackgroundResource(R.color.blue);
+                add_fragment( fragment );
+                //   txt_language.setBackgroundColor(Color.RED);
+                txt_language.setBackgroundResource( R.color.blue );
             }
-        });
-        txt_hotel.setOnClickListener(new View.OnClickListener() {
+        } );
+        txt_hotel.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Fragment fragment = new FragmentHotel();
-                add_fragment(fragment);
-                txt_hotel.setBackgroundResource(R.color.blue);
+                add_fragment( fragment );
+                txt_hotel.setBackgroundResource( R.color.blue );
             }
-        });
-        txt_restaurant.setOnClickListener(new View.OnClickListener() {
+        } );
+        txt_restaurant.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Fragment fragment = new FragmentRestaurant();
-                add_fragment(fragment);
-                txt_restaurant.setBackgroundResource(R.color.blue);
+                add_fragment( fragment );
+                txt_restaurant.setBackgroundResource( R.color.blue );
             }
-        });
-        txt_DiaDiem.setOnClickListener(new View.OnClickListener() {
+        } );
+        txt_taxi.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Fragment fragment = new FragmentDiaDiem();
-                add_fragment(fragment);
-                txt_DiaDiem.setBackgroundResource(R.color.blue);
+                Fragment fragment = new FragmentTaxi();
+                add_fragment( fragment );
+                txt_taxi.setBackgroundResource( R.color.blue );
             }
-        });
+        } );
     }
 
     private void add_fragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_all_service, fragment);
+        fragmentTransaction.replace( R.id.frame_all_service, fragment );
         fragmentTransaction.commit();
     }
 
     private void init() {
-        txt_language = findViewById(R.id.txt_language);
-        txt_hotel = findViewById(R.id.txt_hotel);
-        txt_restaurant=findViewById(R.id.txt_restaurant);
-        txt_DiaDiem = findViewById(R.id.txtDiaDiem);
+        txt_language = findViewById( R.id.txt_language );
+        txt_hotel = findViewById( R.id.txt_hotel );
+        txt_restaurant = findViewById( R.id.txt_restaurant );
+        txt_taxi = findViewById( R.id.txt_taxi );
     }
 }
