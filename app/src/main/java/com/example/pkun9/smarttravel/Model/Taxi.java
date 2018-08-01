fa303cfa82710a8ca8333d45c9325907b587f330
QQ <quochuy.dtvtk7a@gmail.com>
2018-08-01 19:28:41 +0700

    - Thêm DatabaseHandel

--- app/src/main/java/com/example/pkun9/smarttravel/Model/Taxi.java
+++ app/src/main/java/com/example/pkun9/smarttravel/Model/Taxi.java
@@ -1,54 +1,6 @@
 package com.example.pkun9.smarttravel.Model;
 
 /**
-<<<<<<< HEAD
- * @author Quốc Huy
- * @version 1.0.0
- * @description
- * @desc Developer NEO Company.
- * @created 7/2/2018
- * @updated 7/2/2018
- * @modified by
- * @updated on 7/2/2018
- * @since 1.0
- */
-public class Taxi {
-    private String Ten;
-    private String Gia;
-    private String Anh;
-
-    public Taxi(String ten, String gia, String anh) {
-        Ten = ten;
-        Gia = gia;
-        Anh = anh;
-    }
-
-    public Taxi() {
-    }
-
-    public String getTen() {
-        return Ten;
-    }
-
-    public void setTen(String ten) {
-        Ten = ten;
-    }
-
-    public String getGia() {
-        return Gia;
-    }
-
-    public void setGia(String gia) {
-        Gia = gia;
-    }
-
-    public String getAnh() {
-        return Anh;
-    }
-
-    public void setAnh(String anh) {
-        Anh = anh;
-=======
  * @author Nguyễn Anh Phan
  * @version 1.0.0
  * @description
@@ -95,6 +47,5 @@ public class Taxi {
 
     public void setAnh(String anh) {
         this.anh = anh;
->>>>>>> origin/master
     }
 }
